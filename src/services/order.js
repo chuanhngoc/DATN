331f325d7a0e3d4c70f3416876e71028adbaa5a3
chuanhngoc <chuthieugia8102003@gmail.com>
2025-04-22 18:36:40 +0700

    update

--- src/services/order.js
+++ src/services/order.js
@@ -1,3 +1,5 @@
+import { instanceLocal } from "./instance";
+
 // Get orders with optional status filter
 export const getOrders = async (status = '') => {
     try {
@@ -20,4 +22,16 @@ export const getOrderDetail = async (orderId) => {
 
 export const paymentReusult = async (queryString) => {
     return instanceLocal.get(`/vnpay/return?${queryString}`);
+};
+
+// Cancel order with reason
+export const cancelOrder = async (orderId, cancelReason) => {
+    try {
+        const response = await instanceLocal.post(`/orders/history/${orderId}/cancel`, {
+            cancel_reason: cancelReason
+        });
+        return response.data;
+    } catch (error) {
+        throw error.response?.data?.message || 'Có lỗi xảy ra';
+    }
 };
